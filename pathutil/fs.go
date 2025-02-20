0799bdf472279cf336158acc9abf82904f2565ff
obnahsgnaw <obnahsgnaw@qq.com>
2025-02-20 18:06:44 +0800

    perf(x): x

--- pathutil/fs.go
+++ pathutil/fs.go
@@ -4,7 +4,8 @@ import (
 	"embed"
 	"io/fs"
 	"os"
-	"strings"
+	"path"
+	"path/filepath"
 )
 
 func CopyEmbedFsDir(f embed.FS, dirName, rootPath string, replace func(name string, content []byte) (string, []byte)) (err error) {
@@ -15,7 +16,7 @@ func CopyEmbedFsDir(f embed.FS, dirName, rootPath string, replace func(name stri
 	if rpDir == "" {
 		return nil
 	}
-	if err = MkdirAll(filepathJoin(rootPath, rpDir), 0755); err != nil {
+	if err = MkdirAll(path.Join(rootPath, rpDir), 0755); err != nil {
 		return
 	}
 	var dirs []fs.DirEntry
@@ -24,11 +25,11 @@ func CopyEmbedFsDir(f embed.FS, dirName, rootPath string, replace func(name stri
 	}
 	for _, d := range dirs {
 		if d.IsDir() {
-			if err = CopyEmbedFsDir(f, filepathJoin(dirName, d.Name()), rootPath, replace); err != nil {
+			if err = CopyEmbedFsDir(f, path.Join(dirName, d.Name()), rootPath, replace); err != nil {
 				return
 			}
 		} else {
-			fileName := filepathJoin(dirName, d.Name())
+			fileName := path.Join(dirName, d.Name())
 			if err = CopyEmbedFsFile(f, fileName, rootPath, replace); err != nil {
 				return
 			}
@@ -49,12 +50,8 @@ func CopyEmbedFsFile(f embed.FS, fileName, rootPath string, replace func(name st
 	if rpFileName == "" {
 		return
 	}
-	if err = os.WriteFile(filepathJoin(rootPath, rpFileName), rpContent, 0640); err != nil {
+	if err = os.WriteFile(filepath.Join(rootPath, rpFileName), rpContent, 0640); err != nil {
 		return
 	}
 	return
-}
-
-func filepathJoin(name ...string) string {
-	return strings.Join(name, "/")
 }
