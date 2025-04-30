f257945f15429f0247f1987c51b09bd73c796879
obnahsgnaw <obnahsgnaw@qq.com>
2025-04-30 22:06:11 +0800

    perf(x): x

--- errutil/build.go
+++ errutil/build.go
@@ -4,13 +4,30 @@ type Builder struct {
 	prefix string
 }
 
+// NewBuilder
+// Deprecated:
 func NewBuilder(prefix string) *Builder {
 	return &Builder{prefix: prefix}
 }
 
+// Deprecated:
 func (b *Builder) New(err error, desc ...string) error {
 	if err != nil {
 		return NewFromError(err, append([]string{b.prefix}, desc...)...)
 	}
 	return New(append([]string{b.prefix, ": "}, desc...)...)
+}
+
+type ErrBuilder struct {
+	Prefix string
+}
+
+func (b *ErrBuilder) New(err error, desc ...string) error {
+	if b.Prefix == "" {
+		panic("error builder prefix not set")
+	}
+	if err != nil {
+		return NewFromError(err, append([]string{b.Prefix}, desc...)...)
+	}
+	return New(append([]string{b.Prefix, ": "}, desc...)...)
 }
